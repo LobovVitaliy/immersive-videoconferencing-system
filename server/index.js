5b5c46b45156066066e01b1807440992c3340b61
petrokryvenko <37814397+petrokryvenko@users.noreply.github.com>
2020-03-15 12:39:41 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-console.log('server');
+const WebSocket = require('ws');
+
+const WS_PORT = 8081;
+
+const wss = new WebSocket.Server({ port: WS_PORT });
+
+wss.on('connection', function (ws) {
+  ws.on('message', function (message) {
+    wss.clients.forEach(function (client) {
+      if (client !== ws) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+wss.on('listening', function () {
+  console.log('ws server listening on port ' + WS_PORT);
+});
